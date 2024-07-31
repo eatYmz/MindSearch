db1612ac07902fc762b0594bceb08317d1fa8563
eatYmz <117731251+eatYmz@users.noreply.github.com>
2024-07-31 22:36:33 +0800

    Update app.py

--- mindsearch/app.py
+++ mindsearch/app.py
@@ -116,4 +116,4 @@ async def run(request: GenerationParams):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8002, log_level='info')
+    uvicorn.run(app)
